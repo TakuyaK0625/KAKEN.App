a5f8fccb40ebc9fdaba46581892a745a99205667
TakuyaK0625 <37074206+TakuyaK0625@users.noreply.github.com>
2020-04-28 23:06:21 +0900

    Add files via upload

--- raw_data/clean_kubun.R
+++ raw_data/clean_kubun.R
@@ -6,7 +6,9 @@ library(stringr)
 # 審査区分表
 kubun <- fread("kaken_kubun.csv", colClasses = rep("character", 5))
 
-kubun %>% 
-    mutate(中区分 = str_replace(中区分, "およびその関連分野", "")) %>%
-    mutate(小区分 = str_replace(小区分, "関連$", "")) %>%
-    fwrite("../99_cleaned_data/kubun.csv")
+# 審査区分名の整理
+kubun[, 中区分 := str_replace(中区分, "およびその関連分野", "")]
+kubun[, 小区分 := str_replace(小区分, "関連$", "")]
+
+# csvで書き出し
+fwrite(kubun, "../99_cleaned_data/kubun.csv")
--- raw_data/clean_radar.R
+++ raw_data/clean_radar.R
@@ -1,27 +1,20 @@
 library(dplyr)
 library(data.table)
 
+# データのインポート
 D <- fread("../99_cleaned_data/cleaned_df.csv")
 kubun <- fread("../99_cleaned_data/kubun.csv", colClasses = rep("character", 5))
 
-D %>% names
-kubun %>% names
+# 中区分列を作成
+D1 <- merge(D, kubun[, .(小区分, 中区分コード)] %>% unique, by.x = "区分名", by.y = "小区分", all.x = TRUE, allow.cartesian = T)
+D1[, 中区分コード := ifelse(区分 == "中区分", 区分コード, 中区分コード)]
 
-# 元データフレームの作成
-radarD <- D %>% 
-    
-  # 中区分列作成
-  left_join(kubun %>% select(小区分, 中区分コード) %>% unique, by = c("区分名" = "小区分")) %>%
-  mutate(中区分コード = ifelse(区分 == "中区分", 区分コード, 中区分コード)) %>%
-    
-  # 大区分列作成
-  left_join(kubun %>% select(中区分コード, 大区分) %>% unique, by = c("中区分コード" = "中区分コード")) %>%
-  mutate(大区分 = ifelse(区分 == "大区分", 区分名, 大区分)) %>%
-    
-  # 必要な列選択
-  select(年度, 研究課題番号, 所属機関, 研究種目, 直接経費, 区分, 中区分コード, 大区分) 
+# 大区分列作成
+D2 <- merge(D1, kubun[, .(中区分コード, 大区分)] %>% unique, by = "中区分コード", all.x = TRUE, allow.cartesian = T)
+D2[, 大区分 := ifelse(区分 == "大区分", 区分名, 大区分)]
 
 
+D3 <- D2[区分 != "", .(年度, 研究課題番号, 所属機関, 研究種目, 直接経費, 区分, 中区分コード, 大区分)]
 
-
-fwrite(radarD, "../02_radar/radar_df.csv")
+# csvで書き出し
+fwrite(D3, "../02_radar/radar_df.csv")
